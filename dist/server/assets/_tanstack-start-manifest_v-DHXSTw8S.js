7ca2ffaca86a06cb2ca937ac2f968193780ed491
eunsung9476 <eunsung9476@gmail.com>
2026-06-01 08:16:37 +0900

    Delete dist directory

--- dist/server/assets/_tanstack-start-manifest_v-DHXSTw8S.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/관리자/Desktop/lovable-project-heroviz-fixed/src/routes/__root.tsx", children: ["/", "/dashboard"], preloads: ["/assets/index-o89flrOM.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-o89flrOM.js" } }] }, "/": { filePath: "C:/Users/관리자/Desktop/lovable-project-heroviz-fixed/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-Ba0VeGDC.js", "/assets/school-data.functions-DjB0UiHX.js"] }, "/dashboard": { filePath: "C:/Users/관리자/Desktop/lovable-project-heroviz-fixed/src/routes/dashboard.tsx", children: void 0, preloads: ["/assets/dashboard--1XBl54N.js", "/assets/school-data.functions-DjB0UiHX.js"] } } });
-export {
-  tsrStartManifest
-};
